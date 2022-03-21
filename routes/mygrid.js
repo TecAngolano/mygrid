84c849422d441317f011dbb9211cd96c8fe2abcb
FaiscaMalvada <84383797+TecAngolano@users.noreply.github.com>
2022-03-21 12:59:39 -0300

    Add files via upload

---
+++ routes/mygrid.js
@@ -0,0 +1,16 @@
+module.exports = (app) =>{
+    var conexao = require('../config/database')
+    conexao()
+    var mygrid = require('../models/mygrid')
+    app.get('/mygrid',(req,res)=>{
+        res.render('mygrid.ejs')
+    })
+    app.post('/mygrid', (req,res)=>{
+        var documento = new mygrid({
+            titulo:req.body.titulo,
+            texto:req.body.texto
+        }).save()
+        .then(()=>{res.redirect('/mygrid')})
+        .catch(()=>{res.send('Nao foi possivel gravar')})
+    })
+}
